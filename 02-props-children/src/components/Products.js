15a0b71a4e3d6e37eac56cf24894a031db1c6730
japsolo <javier@adboxpublicidad.com>
2020-10-19 11:06:35 -0300

    clase de props y children

---
+++ 02-props-children/src/components/Products.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+function Products ({ children, products }) {
+	return (
+		<React.Fragment>
+			<h2>Listado de productos</h2>
+			<div className="row">
+				{
+					products.map(function (oneProductFinal) {
+						return (
+							<div className="col-6" key={oneProductFinal.id}>
+								<h2>{oneProductFinal.name}</h2>
+								<p>{oneProductFinal.price}</p>
+							</div>
+						)
+					})
+				}
+			</div>
+			{ children }
+		</React.Fragment>
+	)
+}
+
+export default Products;
