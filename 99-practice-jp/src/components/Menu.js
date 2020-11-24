7038fda79b4e8aac4861fab25119acb49387cdcf
japsolo <jh.docente@gmail.com>
2020-11-24 11:02:24 -0300

    refactor router / nav

--- 99-practice-jp/src/components/Menu.js
+++ 99-practice-jp/src/components/Menu.js
@@ -5,15 +5,18 @@ import { faCog, faPlus } from '@fortawesome/free-solid-svg-icons'
 import '../assets/css/Menu.css';
 
 const Menu = props => {
-	// const { movieData } = props;
+  // const { movieData } = props;
+  const { activeSection } = props;
+  console.log(`Estamos en el componente Menu.js y la sección: ${activeSection}`);
 	return (
     <header>
       <nav className="navbar navbar-dark bg-dark">
         <a className="navbar-brand text-white-50" href="setting.html"> {/* Implementar router */}
-          <FontAwesomeIcon icon={faCog} />
+          { activeSection === '/' ? <FontAwesomeIcon icon={faCog} /> : null}
+          { activeSection === 'settings' ? <FontAwesomeIcon icon={faPlus} /> : null}
         </a>
         <span className="navbar-text text-white">
-          My subscriptions
+          {activeSection === 'settings' ? 'Configuraciones' : 'My subscriptions' }
         </span>
         <a className="navbar-brand text-white-50" href="add_existing.html"> {/* Implementar router */}
           <FontAwesomeIcon icon={faPlus} />
