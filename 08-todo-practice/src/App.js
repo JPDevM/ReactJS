6cc31da40ddd9a0a986bb2e1edbdf4de3e52527a
Juan Pablo <devecchi@exxo.es>
2020-12-05 15:54:57 +0100

    new practice

---
+++ 08-todo-practice/src/App.js
@@ -0,0 +1,28 @@
+import React, { useState, useRef } from 'react';
+import TodoList from './TodoList';
+
+function App() {
+  const [ todos, setTodos ] = useState([])
+  const todoNameRef = useRef() 
+  
+  function handleAddTodo() {
+    const name = todoNameRef.current.value
+    if (name === '') return 
+    setTodos(prevTodos => {
+      return [...prevTodos, { id: Math.floor(Math.random() * 100), name: name, complete: false }]
+    })
+    todoNameRef.current.value = null
+  }
+
+  return (
+    <>
+      <TodoList todos={todos} />
+      <input ref={ todoNameRef } type="text" />
+      <button onClick={handleAddTodo}>Add todo</button>
+      <button>Clear completed todos</button>
+      <div>0 left to do</div>
+    </>
+  )
+}
+
+export default App;
