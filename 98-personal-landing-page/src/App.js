bf8b724d0ab06282dbf004f7424d2b4de58cd171
japsolo <jh.docente@gmail.com>
2020-12-22 11:06:01 -0300

    Clase refactor navbar

--- 98-personal-landing-page/src/App.js
+++ 98-personal-landing-page/src/App.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import Container from 'react-bootstrap/Container'
+import React from 'react';
+import Container from 'react-bootstrap/Container';
 
 // Custom components
-import NavBarMenuV2 from './NavBarMenuV2'
-import MainContent from './MainContent'
-import AboutMe from './AboutMe'
-import Experience from './Experience'
-import Work from './Work'
-import Contact from './Contact'
-import Footer from './Footer'
+import NavBarMenuV2 from './NavBarMenuV2';
+import MainContent from './MainContent';
+import AboutMe from './AboutMe';
+import Experience from './Experience';
+import Work from './Work';
+import Contact from './Contact';
+import Footer from './Footer';
 
 const App = () => ( 
   <>
