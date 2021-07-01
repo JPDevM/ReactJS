d460f3535edd90788c96c9c7f45832a9e43d61eb
japsolo <jh.docente@gmail.com>
2021-07-01 10:59:32 -0300

    init with React Front

---
+++ Susbcription-App_React-CLI+BackEnd/client/src/components/Banner.jsx
@@ -0,0 +1,9 @@
+const Banner = () => {
+	return (
+		<div>
+			<h2>Acá va el banner</h2>
+		</div>
+	)
+}
+
+export default Banner;
---
+++ Susbcription-App_React-CLI+BackEnd/client/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import Banner from '../components/Banner.jsx';
+
+const Home = () => {
+	return (
+		<>
+			<Banner/>
+			<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque maxime nam quos sapiente libero! Tempora blanditiis dolores laudantium, quas dicta magni deleniti modi similique, ut labore atque tenetur? Iste, a!</p>
+		</>
+	)
+}
+
+export default Home;
