96ed86542aeae7f16d4f9c7643e98e79b7f02e29
Juan Pablo <devecchi@exxo.es>
2021-07-06 20:56:07 +0200

    work in clean React

--- Subscription-App_React-CLI+BackEnd/client/src/index.js
+++ Subscription-App_React-CLI+BackEnd/client/src/index.js
@@ -15,3 +15,24 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+/* ***********************************************
+_   _              _   _                   _
+| | | | ___ _   _  | |_| |__   ___ _ __ ___| |
+| |_| |/ _ \ | | | | __| '_ \ / _ \ '__/ _ \ |
+|  _  |  __/ |_| | | |_| | | |  __/ | |  __/_|
+|_| |_|\___|\__, |  \__|_| |_|\___|_|  \___(_)
+					 |___/
+
+Looking at the source code, huh?
+Make sure to write to me! I would love to chat
+whether about code, undertakings,
+longboard, anime or anything else.
+
+I leave you the way to contact me
+https://twitter.com/JPDevM --> for random topics
+https://www.linkedin.com/in/juanpablodm/ --> if you need seriousness
+
+- Juan Pablo Devecchi Martinengo or better still call me JP
+
+*********************************************** */
